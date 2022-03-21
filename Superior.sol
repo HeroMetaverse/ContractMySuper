d49defddbae5088798422ee069e6b3819e5c9085
herometaverse <98014158+HeroMetaverse@users.noreply.github.com>
2022-03-21 15:30:30 +0800

    Update Superior.sol

--- Superior.sol
+++ Superior.sol
@@ -8,8 +8,12 @@ contract Superior {
     function setSuperior(address _superior) external {
         require(_superior != msg.sender && _superior != address(0),"can't self or address(0)");
         require( mySuperior[msg.sender] == address(0), "only once!");
+        require(mySuperior[_superior] != address(0) || _superior == address(0xb14eA34AF3D090bbB4f7Ebb8048dbAa1eC0AfC65),"not 0, or xxx");
+        require(mySuperior[_superior] != msg.sender, "ya");
         mySuperior[msg.sender] = _superior;
     }
 }
+
+
 
 
